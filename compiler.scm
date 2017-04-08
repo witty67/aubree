9bd49ce578a141d911db8a7e288bb13862278920
vtomole <vtomole@iastate.edu>
2017-04-08 14:22:02 -0500

    add cnot gate, do i want to add barrier?

--- compiler.scm
+++ compiler.scm
@@ -1,11 +1,12 @@
 #lang racket
 ;;(define program "")
+(define cnot-test "")
 (define output-program "")
 (define test-program "from IBMQuantumExperience import IBMQuantumExperience
 api = IBMQuantumExperience (\"c413835f96f5c9ce144faa39b7cab48d9f7b06cdaa7a5c00f2da33b3c56dbf453680ae1e9fa47cf58329404ab9ba7b8a7f2b85d961a0d4dac5ff2220e42272d1\")
 qasm = 'OPENQASM 2.0;\\n\\ninclude \"qelib1.inc\";\\nqreg q[5];\\ncreg c[5];\\nh q[0];\\ncx q[0],q[2];\\nmeasure q[0] -> c[0];\\nmeasure q[2] -> c[1];\\n'
 print api.run_experiment(qasm, device='simulator', shots=1024, name=None, timeout=60)")
-(define init-register "nqreg q[5];\\ncreg c[5];\\nh q[0];\\n")
+(define init-register "qreg q[5];\\ncreg c[5];\\nh q[0];\\nh q[0];\\nh q[0];\\nh q[0];\\nmeasure q[0] -> c[0];\\n")
 
 
 (define prototype "qreg q[5];\\ncreg c[5];\\nh q[0];\\ncx q[0],q[2];\\nmeasure q[0] -> c[0];\\nmeasure q[2] -> c[1];\\n'")
@@ -48,6 +49,18 @@ print api.run_experiment(qasm, device='simulator', shots=1024, name=None, timeou
   (let ((gate (car expr)))
     (cond
       ((eq? gate 'h) (set! output-program (comma-newline (string-join (string-replace "h q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 'id) (set! output-program (comma-newline (string-join (string-replace "id q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 'x) (set! output-program (comma-newline (string-join (string-replace "x q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 'y) (set! output-program (comma-newline (string-join (string-replace "y q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 'z) (set! output-program (comma-newline (string-join (string-replace "z q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 's) (set! output-program (comma-newline (string-join (string-replace "s q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 's+) (set! output-program (comma-newline (string-join (string-replace "sdg q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 't) (set! output-program (comma-newline (string-join (string-replace "t q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 't+) (set! output-program (comma-newline (string-join (string-replace "tdg q[x]" "x" (number->string (cadr expr)))output-program))))
+      ((eq? gate 'cnot) (begin (set! output-program (string-join (string-replace "cx q[y]," "y" (number->string (cadr expr)))output-program))
+                               (set! output-program (comma-newline (string-join (string-replace "q[x]" "x" (number->string (caddr expr)))output-program)))))
+                               
+      
       (else
        (display "Not valid\n")))
     gate))
