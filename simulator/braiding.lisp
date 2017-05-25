aa685b666fdaeac2cf237ebdff797db8a8364791
vtomole <vtomole@iastate.edu>
2017-05-24 20:49:59 -0500

    finished vector-swap

--- simulator/braiding.lisp
+++ simulator/braiding.lisp
@@ -14,9 +14,17 @@
   (setf (lm:matrix-elt matrix (first final-location) (second final-location))
 	(lm:matrix-elt matrix (first first-location) (second first-location))))
 
-(defun swap (matrix first-location second-location)
+(defun matrix-swap (matrix first-location second-location)
 	   (let ((tmp (lm:matrix-elt matrix (first first-location) (second first-location))))
-		 (setf (lm:matrix-elt matrix (first first-location) (second first-location))  (lm:matrix-elt matrix (first second-location) (second second-location)))
+	     (setf (lm:matrix-elt matrix (first first-location) (second first-location))
+		   (lm:matrix-elt matrix (first second-location) (second second-location)))
 		 (setf (lm:matrix-elt matrix (first second-location) (second second-location)) tmp)
 		 matrix))
+
+(defun vector-swap (vector first-location second-location)
+	   (let ((tmp (aref vector (position first-location vector)))
+		 (tmp-index  (position second-location vector)))
+	     (setf (aref vector (position first-location vector)) (aref vector (position second-location vector)))
+	     (setf (aref vector tmp-index) tmp)
+	     )vector)
   
