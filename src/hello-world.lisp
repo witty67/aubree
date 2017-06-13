b81b77a5686d9f1d5e759146c1a248cffe6494c0
vtomole <vtomole@iastate.edu>
2017-06-13 18:39:04 -0500

    file upload test 2

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -1,6 +1,27 @@
 (in-package :aubree)
 (load "../simulator/qgame.lisp")
 
+;; Utils
+(defun heroku-getenv (target)
+  #+ccl (ccl:getenv target)
+  #+sbcl (sb-posix:getenv target))
+
+(defun db-params ()
+  "Heroku database url format is postgres://username:password@host:port/database_name.
+TODO: cleanup code."
+  (let* ((url (second (cl-ppcre:split "//" (heroku-getenv "DATABASE_URL"))))
+	 (user (first (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
+	 (password (second (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
+	 (host (first (cl-ppcre:split ":" (first (cl-ppcre:split "/" (second (cl-ppcre:split "@" url)))))))
+	 (database (second (cl-ppcre:split "/" (second (cl-ppcre:split "@" url))))))
+    (list database user password host)))
+
+(defun heroku-slug-dir ()
+  (heroku-getenv "HOME"))
+
+;;handler
+(push (hunchentoot:create-folder-dispatcher-and-handler "/static/" (concatenate 'string (heroku-slug-dir) "/public/")) hunchentoot:*dispatch-table*)
+
 
 (defparameter *quantum-program*
 	   `((hadamard 2)
