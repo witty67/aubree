c63046e14f06e25c4dbd19b0e21a188bd77d39a6
vtomole <vtomole@iastate.edu>
2017-07-10 23:09:09 -0500

    see if database works online

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -276,12 +276,13 @@
 (defun db-params ()
   "Heroku database url format is postgres://username:password@host:port/database_name.
 TODO: cleanup code."
+  (if (string= (heroku-getenv "HOME") "/home/vtomole") (list "aubreedb" "vtomole" "19962014V" "localhost")
   (let* ((url (second (cl-ppcre:split "//" (heroku-getenv "DATABASE_URL"))))
 	 (user (first (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
 	 (password (second (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
 	 (host (first (cl-ppcre:split ":" (first (cl-ppcre:split "/" (second (cl-ppcre:split "@" url)))))))
 	 (database (second (cl-ppcre:split "/" (second (cl-ppcre:split "@" url))))))
-    (list database user password host)))
+    (list database user password host))))
 
 ;; Handlers
 (defun publish-static-content ()
